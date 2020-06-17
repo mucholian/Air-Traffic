3e0c5beacd4b701f882093037bc3b2fc929357fa
Moses Rahnama <65908695+mucholian@users.noreply.github.com>
2020-06-17 14:09:27 -0400

    Create bloomberg_data_pull.py
    Bloomberg API. The output is

---
+++ Python Modules/bloomberg_data_pull.py
@@ -0,0 +1,51 @@
+"""
+Pulling data via bloomberg api. Average prices for week ending friday.
+Monthly is also average price for the month. Formulas are below
+"""
+# %%
+import pandas as pd
+import math
+import seaborn as snn
+import numpy as np
+import matplotlib
+import requests
+import sqlite3
+import os
+import datetime
+import sqlalchemy as sql
+import pdblp as bbg
+from xbbg import blp
+
+# %%
+#create bloomberg connection
+con = bbg.BCon(debug=False, port=8194, timeout=5000)
+con.start()
+today = datetime.datetime.now()
+s_date = '2011-01-01'
+e_date = today
+#read ticker list
+names = ['Tickers']
+tix = pd.read_csv("disty_tickers.csv",names=names).values.tolist()
+# create a global df for all prices. index needs to be created for begining of the month
+global_index = pd.date_range(start=s_date,end=e_date, freq='W-FRI')
+global_df = pd.DataFrame(index=global_index)
+
+# %% bloomberg data requests
+
+#create loop here for t in tix
+for tt in tix:
+    test_temp = blp.bdh(tickers=tt,flds=['last_price'],start_date=s_date,
+        end_date=e_date,Quote='G',Per='W', Fill='B', Days='W')
+    #get the name for dataframe header
+    tick_name = blp.bdp(tickers=tt,flds=['Security_Name'])
+    header=str(tt).strip('[]')
+    test_temp.columns=[header]
+    #merge current df with global df
+    global_df=global_df.join(test_temp, how='outer')
+
+global_df.to_csv('bbg_weekly_disty.csv')
+
+
+# %%
+
+
